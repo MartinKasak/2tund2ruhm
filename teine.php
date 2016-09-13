c603b4c72cb61d2d024577e60171b1ee4d0e0ba4
martkasa <martkasa@greeny.cs.tlu.ee>
2016-09-13 13:13:05 +0300

    for loop & if statement

---
+++ teine.php
@@ -0,0 +1,45 @@
+
+
+<?php
+
+	$age = 5;
+	
+	if ($age < 18){
+		
+		echo "alaealine";
+	
+	} else {
+		
+		echo "täisealine";
+		
+	}
+	
+
+
+
+?>
+
+<br>
+
+
+<?php
+
+	//vanusele vastavalt 
+	//trükime välja sõna 'palju'
+	
+	for($i=1 ; $i <= $age ; $i= $i + 1){
+		
+		echo $i."palju ";
+		
+		
+	}
+
+	echo "õnne!";
+
+
+?>
+
+
+
+
+
